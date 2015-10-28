8b9fbf22ba5b808cc780da7dab0a7a264a65bd00
Bart De Smet <bartde@microsoft.com>
2015-10-28 13:42:23 -0700

    Adding initial scaffolding for test project.

---
+++ ExceptionHandlerLowering/Tests/AssertEx.cs
@@ -0,0 +1,37 @@
+﻿// Prototyping support for interpretation of TryExpression with a fault handler or exception filters.
+//
+// bartde - October 2015
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    internal class AssertEx
+    {
+        internal static void Throws<T>(Action action)
+            where T : Exception
+        {
+            Throws<T>(action, _ => true);
+        }
+
+        internal static void Throws<T>(Action action, Func<T, bool> assert)
+            where T : Exception
+        {
+            var failed = false;
+
+            try
+            {
+                action();
+            }
+            catch (T ex)
+            {
+                Assert.IsTrue(assert(ex));
+
+                failed = true;
+            }
+
+            Assert.IsTrue(failed);
+        }
+    }
+}
---
+++ ExceptionHandlerLowering/Tests/Tests.cs
@@ -0,0 +1,21 @@
+﻿// Prototyping support for interpretation of TryExpression with a fault handler or exception filters.
+//
+// bartde - October 2015
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq.Expressions;
+
+namespace Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void LambdaExpressionExtensions_ArgumentChecking()
+        {
+            AssertEx.Throws<ArgumentNullException>(() => LambdaExpressionExtensions.CompileWithExceptionHandling(default(LambdaExpression)));
+            AssertEx.Throws<ArgumentNullException>(() => LambdaExpressionExtensions.CompileWithExceptionHandling(default(Expression<Action>)));
+        }
+    }
+}
