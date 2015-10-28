e7217d8c27723f7d9ad7651eebfd8cd33af8b6fa
Bart De Smet <bartde@microsoft.com>
2015-10-28 13:16:49 -0700

    Refactoring into separate library.

---
+++ ExceptionHandlerLowering/Library/System/Runtime/CompilerServices/ExceptionHandling.cs
@@ -0,0 +1,42 @@
+﻿// Prototyping support for interpretation of TryExpression with a fault handler or exception filters.
+//
+// bartde - October 2015
+
+namespace System.Runtime.CompilerServices
+{
+    public static class ExceptionHandling
+    {
+        public static LeaveHandlerData TryFilter<TException>(Func<LeaveHandlerData> body, Func<TException, bool> filter, Func<TException, LeaveHandlerData> handler)
+            where TException : Exception
+        {
+            try
+            {
+                return body();
+            }
+            catch (TException ex) when (filter(ex))
+            {
+                return handler(ex);
+            }
+        }
+
+        public static LeaveHandlerData TryFault(Func<LeaveHandlerData> body, Action handler)
+        {
+            var result = default(LeaveHandlerData);
+            var success = false;
+            try
+            {
+                result = body();
+                success = true;
+            }
+            finally
+            {
+                if (!success)
+                {
+                    handler();
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ExceptionHandlerLowering/Library/System/Runtime/CompilerServices/LeaveHandlerData.cs
@@ -0,0 +1,18 @@
+﻿// Prototyping support for interpretation of TryExpression with a fault handler or exception filters.
+//
+// bartde - October 2015
+
+namespace System.Runtime.CompilerServices
+{
+    public struct LeaveHandlerData
+    {
+        public LeaveHandlerData(int index, object value)
+        {
+            Index = index;
+            Value = value;
+        }
+
+        public readonly int Index;
+        public readonly object Value;
+    }
+}
