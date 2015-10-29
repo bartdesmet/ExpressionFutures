3074708761575fee0a204b8144d541137074ffbc
Bart De Smet <bartde@microsoft.com>
2015-10-28 22:17:28 -0700

    Adding an API example for Call.

--- CSharpExpressions/Playground/Program.cs
+++ CSharpExpressions/Playground/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using System;
+using System.Linq.Expressions;
 
 namespace Microsoft.CSharp.Expressions
 {
@@ -10,6 +11,20 @@ namespace Microsoft.CSharp.Expressions
     {
         static void Main()
         {
+            Call();
+        }
+
+        static void Call()
+        {
+            var mtd = typeof(Math).GetMethod("Min", new[] { typeof(int), typeof(int) });
+
+            var val1 = mtd.GetParameters()[0];
+            var val2 = mtd.GetParameters()[1];
+
+            var arg0 = CSharpExpression.Bind(val1, Expression.Constant(1));
+            var arg1 = CSharpExpression.Bind(val2, Expression.Constant(2));
+
+            var call = CSharpExpression.Call(mtd, arg1, arg0);
         }
     }
 }
