2ad0c4a55b984193d762477af30501906a323ca4
Bart De Smet <bartde@microsoft.com>
2015-10-28 19:27:39 -0700

    Initial classes.

---
+++ CSharpExpressions/Library/Microsoft/CSharp/Expressions/CSharpExpression.cs
@@ -0,0 +1,21 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using System.Linq.Expressions;
+
+namespace Microsoft.CSharp.Expressions
+{
+    /// <summary>
+    /// Base class for C#-specific expression tree nodes.
+    /// </summary>
+    public abstract class CSharpExpression : Expression
+    {
+        /// <summary>
+        /// Dispatches to the specific visit method for this node type.
+        /// </summary>
+        /// <param name="visitor">The visitor to visit this node with.</param>
+        /// <returns>The result of visiting this node.</returns>
+        protected internal abstract Expression Accept(CSharpExpressionVisitor visitor);
+    }
+}
---
+++ CSharpExpressions/Library/Microsoft/CSharp/Expressions/CSharpExpressionVisitor.cs
@@ -0,0 +1,35 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using System.Linq.Expressions;
+
+namespace Microsoft.CSharp.Expressions
+{
+    /// <summary>
+    /// Represents a visitor or rewriter for C# expression trees.
+    /// </summary>
+    /// <remarks>
+    /// This class is designed to be inherited to create more specialized
+    /// classes whose functionality requires traversing, examining or copying
+    /// an expression tree.
+    /// </remarks>
+    public abstract class CSharpExpressionVisitor : ExpressionVisitor
+    {
+        /// <summary>
+        /// Visits the children of the extension expression.
+        /// </summary>
+        /// <param name="node">The expression to visit.</param>
+        /// <returns>The modified expression, if it or any subexpression was modified; otherwise, returns the original expression.</returns>
+        protected override Expression VisitExtension(Expression node)
+        {
+            var csharpExpression = node as CSharpExpression;
+            if (csharpExpression != null)
+            {
+                return csharpExpression.Accept(this);
+            }
+
+            return base.VisitExtension(node);
+        }
+    }
+}
