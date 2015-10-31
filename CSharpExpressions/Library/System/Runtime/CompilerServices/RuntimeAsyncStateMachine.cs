c3e5ef4c855a35dd3fff39b1f53aef88a4d007bb
Bart De Smet <bartde@microsoft.com>
2015-10-30 21:20:16 -0700

    Adding a runtime state machine to emit code against.

---
+++ CSharpExpressions/Library/System/Runtime/CompilerServices/RuntimeAsyncStateMachine.cs
@@ -0,0 +1,40 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// This API supports the product infrastructure and is not intended to be used directly from your code.
+    /// Provides a runtime implementation of IAsyncStateMachine usable by runtime code generators.
+    /// </summary>
+    public class RuntimeAsyncStateMachine : IAsyncStateMachine
+    {
+        private readonly Action _moveNext;
+
+        /// <summary>
+        /// Creates a new runtim async state machine using the specified MoveNext implementation delagate.
+        /// </summary>
+        /// <param name="moveNext">Delegte to implement the MoveNext method.</param>
+        public RuntimeAsyncStateMachine(Action moveNext)
+        {
+            _moveNext = moveNext;
+        }
+
+        /// <summary>
+        /// Advances the state machine.
+        /// </summary>
+        public void MoveNext()
+        {
+            _moveNext();
+        }
+
+        /// <summary>
+        /// Set the state machine.
+        /// </summary>
+        /// <param name="stateMachine">State machine.</param>
+        public void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+        }
+    }
+}
