e95d01b7292a20d2d605c3c9c926adc433b4b947
Bart De Smet <bartde@microsoft.com>
2015-10-31 23:40:22 -0700

    Adding ValidateCondition.

---
+++ CSharpExpressions/Library/Microsoft/CSharp/Expressions/CSharpExpression.Helpers.cs
@@ -0,0 +1,26 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using System.Linq.Expressions;
+using static System.Linq.Expressions.ExpressionStubs;
+using LinqError = System.Linq.Expressions.Error;
+
+namespace Microsoft.CSharp.Expressions
+{
+    partial class CSharpExpression
+    {
+        private static void ValidateCondition(Expression test)
+        {
+            RequiresCanRead(test, nameof(test));
+
+            // TODO: We can be more flexible and allow the rules in C# spec 7.20.
+            //       Note that this behavior is the same as IfThen, but we could also add C# specific nodes for those,
+            //       with the more flexible construction behavior.
+            if (test.Type != typeof(bool))
+            {
+                throw LinqError.ArgumentMustBeBoolean();
+            }
+        }
+    }
+}
--- CSharpExpressions/Library/Microsoft/CSharp/Expressions/ConditionalLoopCSharpStatement.cs
+++ CSharpExpressions/Library/Microsoft/CSharp/Expressions/ConditionalLoopCSharpStatement.cs
@@ -3,8 +3,6 @@
 // bartde - October 2015
 
 using System.Linq.Expressions;
-using static System.Linq.Expressions.ExpressionStubs;
-using LinqError = System.Linq.Expressions.Error;
 
 namespace Microsoft.CSharp.Expressions
 {
@@ -29,15 +27,7 @@ namespace Microsoft.CSharp.Expressions
     {
         private static void ValidateLoop(Expression test, Expression body, ref LabelTarget @break, LabelTarget @continue)
         {
-            RequiresCanRead(test, nameof(test));
-            
-            // TODO: We can be more flexible and allow the rules in C# spec 7.20.
-            //       Note that this behavior is the same as IfThen, but we could also add C# specific nodes for those,
-            //       with the more flexible construction behavior.
-            if (test.Type != typeof(bool))
-            {
-                throw LinqError.ArgumentMustBeBoolean();
-            }
+            ValidateCondition(test);
 
             ValidateLoop(body, ref @break, @continue);
         }
