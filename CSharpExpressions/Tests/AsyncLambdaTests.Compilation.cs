69eb2654fbcdfe3c98b81c6773bd9e0b8d78841a
Bart De Smet <bartde@microsoft.com>
2015-11-01 03:04:22 -0800

    Adding simple tests for AsyncLambda.

---
+++ CSharpExpressions/Tests/AsyncLambdaTests.Compilation.cs
@@ -0,0 +1,77 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using Microsoft.CSharp.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    partial class AsyncLambdaTests
+    {
+        [TestMethod]
+        public void AsyncLambda_Compilation_Simple1()
+        {
+            var e = CSharpExpression.AsyncLambda<Func<Task<int>>>(Expression.Constant(42));
+            var f = e.Compile();
+            var t = f();
+            var r = t.Result;
+            Assert.AreEqual(42, r);
+        }
+
+        [TestMethod]
+        public void AsyncLambda_Compilation_Simple2()
+        {
+            var e = CSharpExpression.AsyncLambda<Func<Task>>(Expression.Constant(42));
+            var f = e.Compile();
+            var t = f();
+            t.Wait();
+        }
+
+        [TestMethod]
+        public void AsyncLambda_Compilation_Simple3()
+        {
+            var log = new List<string>();
+            var a = (Expression<Action>)(() => log.Add("OK"));
+
+            var e = CSharpExpression.AsyncLambda<Func<Task>>(a.Body);
+            var f = e.Compile();
+            var t = f();
+
+            // Add happens on sync code path
+            Assert.IsTrue(new[] { "OK" }.SequenceEqual(log));
+
+            t.Wait();
+        }
+
+        [TestMethod]
+        public void AsyncLambda_Compilation_Simple4()
+        {
+            var log = new List<string>();
+            var a = (Expression<Action>)(() => log.Add("OK"));
+
+            var e = CSharpExpression.AsyncLambda<Action>(a.Body);
+            var f = e.Compile();
+            f();
+
+            // Add happens on sync code path
+            Assert.IsTrue(new[] { "OK" }.SequenceEqual(log));
+        }
+
+        [TestMethod]
+        public void AsyncLambda_Compilation_Simple5()
+        {
+            var v = Expression.Constant(Task.FromResult(42));
+            var e = CSharpExpression.AsyncLambda<Func<Task<int>>>(CSharpExpression.Await(v));
+            var f = e.Compile();
+            var t = f();
+            var r = t.Result;
+            Assert.AreEqual(42, r);
+        }
+    }
+}
---
+++ CSharpExpressions/Tests/AsyncLambdaTests.cs
@@ -0,0 +1,13 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public partial class AsyncLambdaTests
+    {
+    }
+}
