3448baf230b9dede3c7f612ed328b2b1a422fa6f
Bart De Smet <bartde@microsoft.com>
2015-11-01 03:26:49 -0800

    Tests for stack spilling and variable hosting in AsyncLambda compilation.

--- CSharpExpressions/Tests/AsyncLambdaTests.Compilation.cs
+++ CSharpExpressions/Tests/AsyncLambdaTests.Compilation.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using static Tests.ReflectionUtils;
 
 namespace Tests
 {
@@ -72,6 +73,52 @@ namespace Tests
             var t = f();
             var r = t.Result;
             Assert.AreEqual(42, r);
+        }
+
+        [TestMethod]
+        public void AsyncLambda_Compilation_Spilling()
+        {
+            var v1 = Expression.Constant(Task.FromResult(1));
+            var v2 = Expression.Constant(Task.FromResult(2));
+            var add = Expression.Add(CSharpExpression.Await(v1), CSharpExpression.Await(v2));
+            var e = CSharpExpression.AsyncLambda<Func<Task<int>>>(add);
+            var f = e.Compile();
+            var t = f();
+            var r = t.Result;
+            Assert.AreEqual(3, r);
+        }
+
+        [TestMethod]
+        public void AsyncLambda_Compilation_Hoisting()
+        {
+            var fromResultMethod = MethodInfoOf(() => Task.FromResult(default(int)));
+            var i = Expression.Parameter(typeof(int));
+            var res = Expression.Parameter(typeof(int));
+            var brk = Expression.Label();
+            var e = CSharpExpression.AsyncLambda<Func<Task<int>>>(
+                Expression.Block(
+                    new[] { i, res },
+                    Expression.Assign(i, Expression.Constant(0)),
+                    Expression.Loop(
+                        Expression.Block(
+                            Expression.IfThen(
+                                Expression.Equal(i, Expression.Constant(10)),
+                                Expression.Break(brk)
+                            ),
+                            Expression.AddAssign(
+                                res,
+                                CSharpExpression.Await(Expression.Call(fromResultMethod, i))
+                            ),
+                            Expression.PostIncrementAssign(i)
+                        ), brk
+                    ),
+                    res
+                )
+            );
+            var f = e.Compile();
+            var t = f();
+            var r = t.Result;
+            Assert.AreEqual(Enumerable.Range(0, 10).Sum(), r);
         }
     }
 }
