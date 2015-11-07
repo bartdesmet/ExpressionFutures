65142fa70b4093f85d63f84a8c71bbee23a4988b
Bart De Smet <bartde@microsoft.com>
2015-11-07 03:42:40 -0800

    Adding a typed label rewriter.

---
+++ CSharpExpressions/Library/Microsoft/CSharp/Expressions/TypedLabelRewriter.cs
@@ -0,0 +1,150 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using System.Collections.Generic;
+using System.Dynamic.Utils;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Microsoft.CSharp.Expressions
+{
+    /// <summary>
+    /// Typed labels can cause issues for async method rewrites. This rewriter gets rid of them by storing
+    /// their result in variables and using void-typed labels instead.
+    /// </summary>
+    /// <remarks>
+    /// An example of a rewrite that's problematic is this:
+    /// <code>
+    ///   var label = Expression.Label(typeof(int));
+    ///   var expr =
+    ///     Expression.Block(
+    ///       Expression.Return(label, Expression.Constant(42)),
+    ///       Expression.Label(label, Expression.Constant(0))
+    ///     );
+    /// </code>
+    /// The problem pops up when performing assignment percolation when we push down assignments in the
+    /// outer try block generated for async methods. This causes the last statement in the block to turn
+    /// into the rhs of an assignment, which is invalid for labels.
+    /// </remarks>
+    internal class TypedLabelRewriter : ShallowVisitor
+    {
+        // NB: Only have to deal with jumps to labels defined in outer blocks; cf. LabelInfo.ValidateJump.
+        // TODO: Check cases where we have shadowing, e.g. with Loop.
+
+        private readonly Stack<IDictionary<LabelTarget, LabelInfo>> _labels = new Stack<IDictionary<LabelTarget, LabelInfo>>();
+
+        protected override Expression VisitBlock(BlockExpression node)
+        {
+            var map = default(IDictionary<LabelTarget, LabelInfo>);
+
+            foreach (var e in node.Expressions)
+            {
+                if (e.NodeType == ExpressionType.Label)
+                {
+                    var l = (LabelExpression)e;
+                    if (l.Type != typeof(void))
+                    {
+                        if (map == null)
+                        {
+                            map = new Dictionary<LabelTarget, LabelInfo>();
+                        }
+
+                        var name = l.Target.Name;
+                        var newLabel = Expression.Label(typeof(void), name);
+                        var labelValue = Expression.Parameter(l.Type, name);
+
+                        var info = new LabelInfo
+                        {
+                            Target = newLabel,
+                            Value = labelValue,
+                        };
+
+                        map.Add(l.Target, info);
+                    }
+                }
+            }
+
+            if (map != null)
+            {
+                _labels.Push(map);
+            }
+
+            var res = (BlockExpression)base.VisitBlock(node);
+
+            if (map != null)
+            {
+                _labels.Pop();
+
+                var newVariables = map.Values.Map(i => i.Value);
+                res = res.Update(res.Variables.Concat(newVariables), res.Expressions);
+            }
+
+            return res;
+        }
+
+        protected override Expression VisitLabel(LabelExpression node)
+        {
+            var info = default(LabelInfo);
+            if (TryGetLabelInfo(node.Target, out info))
+            {
+                var variable = info.Value;
+                var newTarget = info.Target;
+
+                var res =
+                    Expression.Block(
+                        Expression.Assign(variable, node.DefaultValue),
+                        Expression.Label(newTarget),
+                        variable
+                    );
+
+                return res;
+            }
+
+            return base.VisitLabel(node);
+        }
+
+        protected override Expression VisitGoto(GotoExpression node)
+        {
+            var info = default(LabelInfo);
+            if (TryGetLabelInfo(node.Target, out info))
+            {
+                var variable = info.Value;
+                var newTarget = info.Target;
+
+                var res =
+                    Expression.Block(
+                        Expression.Assign(variable, node.Value),
+                        Expression.Goto(newTarget, node.Type)
+                    );
+
+                return res;
+            }
+
+            return base.VisitGoto(node);
+        }
+
+        private bool TryGetLabelInfo(LabelTarget target, out LabelInfo info)
+        {
+            if (target.Type != typeof(void))
+            {
+                foreach (var scope in _labels)
+                {
+                    if (scope.TryGetValue(target, out info))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            info = default(LabelInfo);
+            return false;
+        }
+
+        struct LabelInfo
+        {
+            public LabelTarget Target;
+            public ParameterExpression Value;
+        }
+    }
+}
---
+++ CSharpExpressions/Tests/TypedLabelRewriterTests.cs
@@ -0,0 +1,52 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using Microsoft.CSharp.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq.Expressions;
+
+namespace Tests
+{
+    [TestClass]
+    public class TypedLabelRewriterTests
+    {
+        [TestMethod]
+        public void TypedLabelRewriter_Simple()
+        {
+            var rw = new TypedLabelRewriter();
+
+            var l = Expression.Label(typeof(int));
+            var m = Expression.Label();
+
+            var p = Expression.Parameter(typeof(bool));
+            var e =
+                Expression.Block(
+                    Expression.Goto(m),
+                    Expression.Label(m),
+                    Expression.Block(
+                        Expression.IfThen(
+                            p,
+                            Expression.Goto(l, Expression.Constant(42))
+                        )
+                    ),
+                    Expression.Label(l, Expression.Constant(0))
+                );
+
+            var f1 = Expression.Lambda<Func<bool, int>>(e, p);
+            var d1 = f1.Compile();
+
+            var r = rw.Visit(e);
+
+            var f2 = Expression.Lambda<Func<bool, int>>(r, p);
+            var d2 = f2.Compile();
+
+            Assert.AreEqual(d1(false), d2(false));
+            Assert.AreEqual(d1(true), d2(true));
+        }
+
+        // TODO: add test for nested cases
+        // TODO: add test for branching out of exception handlers etc.
+    }
+}
