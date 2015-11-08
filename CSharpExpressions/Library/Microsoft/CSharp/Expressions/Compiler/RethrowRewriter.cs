d18ce4c003264822f1fb5931ade72ef792d1ca63
Bart De Smet <bartde@microsoft.com>
2015-11-08 05:45:49 -0800

    Adding a rewriter for rethrow statements.

---
+++ CSharpExpressions/Library/Microsoft/CSharp/Expressions/Compiler/RethrowRewriter.cs
@@ -0,0 +1,36 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using System.Linq.Expressions;
+
+namespace Microsoft.CSharp.Expressions.Compiler
+{
+    /// <summary>
+    /// Utility to rewrite rethrow statements.
+    /// </summary>
+    internal class RethrowRewriter : ShallowVisitor
+    {
+        private readonly Expression _replacement;
+
+        public RethrowRewriter(Expression replacement)
+        {
+            _replacement = replacement;
+        }
+
+        protected override CatchBlock VisitCatchBlock(CatchBlock node)
+        {
+            return node; // NB: In nested catch blocks, the meaning of a rethrow changes.
+        }
+
+        protected override Expression VisitUnary(UnaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Throw && node.Operand == null)
+            {
+                return _replacement;
+            }
+
+            return base.VisitUnary(node);
+        }
+    }
+}
