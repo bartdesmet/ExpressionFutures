1a8afbe4baab7106ec948cb21ba9833c8c9274e8
Bart De Smet <bartde@microsoft.com>
2015-11-10 09:20:58 -0800

    Improving and testing assignment percolation.

--- CSharpExpressions/Library/Microsoft/CSharp/Expressions/Compiler/AssignmentPercolator.cs
+++ CSharpExpressions/Library/Microsoft/CSharp/Expressions/Compiler/AssignmentPercolator.cs
@@ -2,6 +2,7 @@
 //
 // bartde - October 2015
 
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
@@ -90,6 +91,11 @@ namespace Microsoft.CSharp.Expressions.Compiler
             return Impl.Instance.Visit(expression);
         }
 
+        // NB: We really only need percolation for node types we introduce during the rewrite steps for async lambdas.
+        //     For example, we introduce Block nodes at will in various places, and introduce Conditional nodes when
+        //     rewriting non-void Try expressions. For completeness, a few other node types are handled below as well,
+        //     which can come in handy later.
+
         class Impl : CSharpExpressionVisitor
         {
             public static readonly ExpressionVisitor Instance = new Impl();
@@ -101,7 +107,7 @@ namespace Microsoft.CSharp.Expressions.Compiler
                 if (res.NodeType == ExpressionType.Assign)
                 {
                     var b = (BinaryExpression)res;
-                    if (b.Right.NodeType == ExpressionType.Block && b.Conversion == null)
+                    if (ShouldPercolate(b.Right.NodeType) && b.Conversion == null)
                     {
                         res = Percolate(b.Left, b.Right);
                     }
@@ -112,17 +118,99 @@ namespace Microsoft.CSharp.Expressions.Compiler
 
             private static Expression Percolate(Expression result, Expression expr)
             {
-                if (expr.NodeType == ExpressionType.Block)
+                switch (expr.NodeType)
                 {
-                    var block = (BlockExpression)expr;
-                    var n = block.Expressions.Count;
-                    var res = Percolate(result, block.Expressions[n - 1]);
-                    return block.Update(block.Variables, block.Expressions.Take(n - 1).Concat(new[] { res }));
+                    // TODO: Loop
+                    case ExpressionType.Block:
+                        {
+                            var block = (BlockExpression)expr;
+
+                            var n = block.Expressions.Count;
+                            var res = Percolate(result, block.Expressions[n - 1]);
+
+                            return block.Update(block.Variables, block.Expressions.Take(n - 1).Concat(new[] { res }));
+                        }
+                    case ExpressionType.Conditional:
+                        {
+                            var cond = (ConditionalExpression)expr;
+
+                            var ifTrue = Percolate(result, cond.IfTrue);
+                            var ifFalse = Percolate(result, cond.IfFalse);
+
+                            return cond.Update(cond.Test, ifTrue, ifFalse);
+                        }
+                    case ExpressionType.Try:
+                        {
+                            var @try = (TryExpression)expr;
+
+                            var body = Percolate(result, @try.Body);
+
+                            var oldHandlers = @try.Handlers;
+                            var newHandlers = (IEnumerable<CatchBlock>)oldHandlers;
+
+                            var n = oldHandlers.Count;
+                            if (n > 0)
+                            {
+                                var handlers = new List<CatchBlock>(n);
+
+                                for (var i = 0; i < n; i++)
+                                {
+                                    var handler = oldHandlers[i];
+
+                                    var catchBody = Percolate(result, handler.Body);
+
+                                    var newHandler = handler.Update(handler.Variable, handler.Filter, catchBody);
+                                    handlers.Add(newHandler);
+                                }
+
+                                newHandlers = handlers;
+                            }
+
+                            return @try.Update(body, newHandlers, @try.Finally, @try.Fault);
+                        }
+                    case ExpressionType.Switch:
+                        {
+                            var @switch = (SwitchExpression)expr;
+
+                            var defaultBody = Percolate(result, @switch.DefaultBody);
+
+                            var oldCases = @switch.Cases;
+                            var n = oldCases.Count;
+                            var newCases = new List<SwitchCase>(n);
+
+                            for (var i = 0; i < n; i++)
+                            {
+                                var @case = oldCases[i];
+
+                                var caseBody = Percolate(result, @case.Body);
+
+                                var newCase = @case.Update(@case.TestValues, caseBody);
+                                newCases.Add(newCase);
+                            }
+
+                            return @switch.Update(@switch.SwitchValue, newCases, defaultBody);
+                        }
+                    default:
+                        return Expression.Assign(result, expr);
                 }
-                else
+            }
+
+            private static bool ShouldPercolate(ExpressionType type)
+            {
+                var res = false;
+
+                switch (type)
                 {
-                    return Expression.Assign(result, expr);
+                    // TODO: Loop
+                    case ExpressionType.Block:
+                    case ExpressionType.Conditional:
+                    case ExpressionType.Try:
+                    case ExpressionType.Switch:
+                        res = true;
+                        break;
                 }
+
+                return res;
             }
         }
     }
---
+++ CSharpExpressions/Tests/AssignmentPercolatorTests.cs
@@ -0,0 +1,110 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - October 2015
+
+using Microsoft.CSharp.Expressions.Compiler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq.Expressions;
+
+namespace Tests
+{
+    [TestClass]
+    public class AssignmentPercolatorTests
+    {
+        [TestMethod]
+        public void AssignmentPercolator_Block()
+        {
+            var b = Expression.Block(Expression.Empty(), Expression.Constant(42));
+            var v = Expression.Parameter(typeof(int));
+            var e = Expression.Assign(v, b);
+            var r = AssignmentPercolator.Percolate(e);
+
+            Assert.AreEqual(ExpressionType.Block, r.NodeType);
+            var t = (BlockExpression)r;
+            Assert.AreSame(b.Expressions[0], t.Expressions[0]);
+            Assert.AreEqual(ExpressionType.Assign, t.Expressions[1].NodeType);
+            var a = (BinaryExpression)t.Expressions[1];
+            Assert.AreSame(v, a.Left);
+            Assert.AreSame(b.Expressions[1], a.Right);
+        }
+
+        [TestMethod]
+        public void AssignmentPercolator_Conditional()
+        {
+            var c = Expression.Condition(Expression.Constant(true), Expression.Constant(42), Expression.Constant(43));
+            var v = Expression.Parameter(typeof(int));
+            var e = Expression.Assign(v, c);
+            var r = AssignmentPercolator.Percolate(e);
+
+            Assert.AreEqual(ExpressionType.Conditional, r.NodeType);
+            var t = (ConditionalExpression)r;
+            Assert.AreSame(c.Test, t.Test);
+            Assert.AreEqual(ExpressionType.Assign, t.IfTrue.NodeType);
+            Assert.AreEqual(ExpressionType.Assign, t.IfFalse.NodeType);
+            var a1 = (BinaryExpression)t.IfTrue;
+            var a2 = (BinaryExpression)t.IfFalse;
+            Assert.AreSame(v, a1.Left);
+            Assert.AreSame(v, a2.Left);
+            Assert.AreSame(c.IfTrue, a1.Right);
+            Assert.AreSame(c.IfFalse, a2.Right);
+        }
+
+        [TestMethod]
+        public void AssignmentPercolator_TryFinally()
+        {
+            var c = Expression.TryFinally(Expression.Constant(42), Expression.Empty());
+            var v = Expression.Parameter(typeof(int));
+            var e = Expression.Assign(v, c);
+            var r = AssignmentPercolator.Percolate(e);
+
+            Assert.AreEqual(ExpressionType.Try, r.NodeType);
+            var t = (TryExpression)r;
+            Assert.AreSame(c.Finally, t.Finally);
+            Assert.AreEqual(ExpressionType.Assign, t.Body.NodeType);
+            var a = (BinaryExpression)t.Body;
+            Assert.AreSame(v, a.Left);
+            Assert.AreSame(c.Body, a.Right);
+        }
+
+        [TestMethod]
+        public void AssignmentPercolator_TryCatch()
+        {
+            var c = Expression.TryCatch(Expression.Constant(42), Expression.Catch(typeof(Exception), Expression.Constant(43)));
+            var v = Expression.Parameter(typeof(int));
+            var e = Expression.Assign(v, c);
+            var r = AssignmentPercolator.Percolate(e);
+
+            Assert.AreEqual(ExpressionType.Try, r.NodeType);
+            var t = (TryExpression)r;
+            Assert.AreEqual(ExpressionType.Assign, t.Body.NodeType);
+            Assert.AreEqual(ExpressionType.Assign, t.Handlers[0].Body.NodeType);
+            var a1 = (BinaryExpression)t.Body;
+            var a2 = (BinaryExpression)t.Handlers[0].Body;
+            Assert.AreSame(v, a1.Left);
+            Assert.AreSame(v, a2.Left);
+            Assert.AreSame(c.Body, a1.Right);
+            Assert.AreSame(c.Handlers[0].Body, a2.Right);
+        }
+
+        [TestMethod]
+        public void AssignmentPercolator_Switch()
+        {
+            var c = Expression.Switch(Expression.Constant(42), Expression.Constant(43), Expression.SwitchCase(Expression.Constant(44), Expression.Constant(45)));
+            var v = Expression.Parameter(typeof(int));
+            var e = Expression.Assign(v, c);
+            var r = AssignmentPercolator.Percolate(e);
+
+            Assert.AreEqual(ExpressionType.Switch, r.NodeType);
+            var t = (SwitchExpression)r;
+            Assert.AreEqual(ExpressionType.Assign, t.DefaultBody.NodeType);
+            Assert.AreEqual(ExpressionType.Assign, t.Cases[0].Body.NodeType);
+            var a1 = (BinaryExpression)t.DefaultBody;
+            var a2 = (BinaryExpression)t.Cases[0].Body;
+            Assert.AreSame(v, a1.Left);
+            Assert.AreSame(v, a2.Left);
+            Assert.AreSame(c.DefaultBody, a1.Right);
+            Assert.AreSame(c.Cases[0].Body, a2.Right);
+        }
+    }
+}
