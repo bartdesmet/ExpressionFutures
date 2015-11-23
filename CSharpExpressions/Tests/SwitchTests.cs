d67413b059b030367884bf88b4400cae85d3e5d1
Bart De Smet <bartde@microsoft.com>
2015-11-22 22:51:20 -0800

    Initial test for switch compilation.

---
+++ CSharpExpressions/Tests/SwitchTests.cs
@@ -0,0 +1,83 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - November 2015
+
+using Microsoft.CSharp.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using static Tests.TestHelpers;
+using System.Collections;
+
+namespace Tests
+{
+    [TestClass]
+    public class SwitchTests
+    {
+        [TestMethod]
+        public void Switch_Compile_Int32()
+        {
+            AssertCompile<int>((log, v) =>
+                SwitchLogValue(log,
+                    v,
+                    CSharpStatement.SwitchCase(log("A"), 1)
+                ),
+                new Asserts<int>
+                {
+                    { 0, "E" },
+                    { 1, "E", "A" },
+                    { 2, "E" },
+                }
+            );
+        }
+
+        private static SwitchCSharpStatement SwitchLogValue(Func<string, Expression> log, Expression expression, params CSharpSwitchCase[] cases)
+        {
+            var b = Expression.Label();
+            var eval = Expression.Block(log("E"), expression);
+            return CSharpStatement.Switch(eval, b, cases);
+        }
+
+        private void AssertCompile<TSwitchValue>(Func<Func<string, Expression>, ParameterExpression, Expression> createExpression, Asserts<TSwitchValue> expected)
+        {
+            var res = WithLog<TSwitchValue>(createExpression).Compile();
+
+            foreach (var c in expected.Cases)
+            {
+                Assert.AreEqual(new LogAndResult<object>(c.Value, null, null), res(c.Key));
+            }
+        }
+
+        private void AssertCompile<TSwitchValue>(Func<Func<string, Expression>, Expression, ParameterExpression, Expression> createExpression, Asserts<TSwitchValue> expected)
+        {
+            var res = WithLog<TSwitchValue>(createExpression).Compile();
+
+            foreach (var c in expected.Cases)
+            {
+                Assert.AreEqual(new LogAndResult<object>(c.Value, null, null), res(c.Key));
+            }
+        }
+
+        class Asserts<TValue> : IEnumerable<object>
+        {
+            public Dictionary<TValue, List<string>> Cases = new Dictionary<TValue, List<string>>();
+
+            public void Add(TValue value, params string[] logs)
+            {
+                Cases.Add(value, logs.ToList());
+            }
+
+            public IEnumerator<object> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- CSharpExpressions/Tests/TestHelpers.cs
+++ CSharpExpressions/Tests/TestHelpers.cs
@@ -21,6 +21,16 @@ namespace Tests
             return WithLog<object>(log => Expression.Block(createExpression(log), Expression.Default(typeof(object))));
         }
 
+        public static Expression<Func<TArgs, LogAndResult<object>>> WithLog<TArgs>(Func<Func<string, Expression>, Expression, ParameterExpression, Expression> createExpression)
+        {
+            return WithLog<TArgs, object>((log, append, param) => Expression.Block(createExpression(log, append, param), Expression.Default(typeof(object))));
+        }
+
+        public static Expression<Func<TArgs, LogAndResult<object>>> WithLog<TArgs>(Func<Func<string, Expression>, ParameterExpression, Expression> createExpression)
+        {
+            return WithLog<TArgs, object>((log, param) => Expression.Block(createExpression(log, param), Expression.Default(typeof(object))));
+        }
+
         public static Expression<Func<LogAndResult<T>>> WithLog<T>(Func<Func<string, Expression>, Expression, Expression> createExpression)
         {
             return WithLogValue<T>((log, append) => createExpression(s => log(Expression.Empty(), s), append));
@@ -29,6 +39,16 @@ namespace Tests
         public static Expression<Func<LogAndResult<T>>> WithLog<T>(Func<Func<string, Expression>, Expression> createExpression)
         {
             return WithLogValue<T>(log => createExpression(s => log(Expression.Empty(), s)));
+        }
+
+        public static Expression<Func<TArgs, LogAndResult<TResult>>> WithLog<TArgs, TResult>(Func<Func<string, Expression>, Expression, ParameterExpression, Expression> createExpression)
+        {
+            return WithLogValue<TArgs, TResult>((log, append, param) => createExpression(s => log(Expression.Empty(), s), append, param));
+        }
+
+        public static Expression<Func<TArgs, LogAndResult<TResult>>> WithLog<TArgs, TResult>(Func<Func<string, Expression>, ParameterExpression, Expression> createExpression)
+        {
+            return WithLogValue<TArgs, TResult>((log, param) => createExpression(s => log(Expression.Empty(), s), param));
         }
 
         public static Expression<Func<LogAndResult<object>>> WithLogValue(Func<Func<Expression, string, Expression>, Expression, Expression> createExpression)
@@ -46,14 +66,19 @@ namespace Tests
             return WithLogValue<T>((log, append) => createExpression(log));
         }
 
-        public static Expression<Func<LogAndResult<T>>> WithLogValue<T>(Func<Func<Expression, string, Expression>, Expression, Expression> createExpression)
+        public static Expression<Func<TArg, LogAndResult<TResult>>> WithLogValue<TArg, TResult>(Func<Func<Expression, string, Expression>, ParameterExpression, Expression> createExpression)
+        {
+            return WithLogValue<TArg, TResult>((log, append, p) => createExpression(log, p));
+        }
+
+        public static Expression<Func<LogAndResult<TResult>>> WithLogValue<TResult>(Func<Func<Expression, string, Expression>, Expression, Expression> createExpression)
         {
             var logParam = Expression.Parameter(typeof(List<string>), "log");
-            var valueParam = Expression.Parameter(typeof(T), "result");
+            var valueParam = Expression.Parameter(typeof(TResult), "result");
             var errorParam = Expression.Parameter(typeof(Exception), "error");
             var exParam = Expression.Parameter(typeof(Exception), "ex");
             var addMethod = logParam.Type.GetMethod("Add", new[] { typeof(string) });
-            var logAndResultCtor = typeof(LogAndResult<T>).GetConstructor(new[] { typeof(List<string>), typeof(T), typeof(Exception) });
+            var logAndResultCtor = typeof(LogAndResult<TResult>).GetConstructor(new[] { typeof(List<string>), typeof(TResult), typeof(Exception) });
 
             var getLogEntry = new Func<Expression, string, Expression>((e, s) => Expression.Block(Expression.Call(logParam, addMethod, Expression.Constant(s, typeof(string))), e));
             var entryParam = Expression.Parameter(typeof(string));
@@ -70,7 +95,7 @@ namespace Tests
 #endif
 
             var res =
-                Expression.Lambda<Func<LogAndResult<T>>>(
+                Expression.Lambda<Func<LogAndResult<TResult>>>(
                     Expression.Block(
                         new[]
                         {
@@ -95,6 +120,62 @@ namespace Tests
                         ),
                         Expression.New(logAndResultCtor, logParam, valueParam, errorParam)
                     )
+                );
+
+            return res;
+        }
+
+        public static Expression<Func<TArgs, LogAndResult<TResult>>> WithLogValue<TArgs, TResult>(Func<Func<Expression, string, Expression>, Expression, ParameterExpression, Expression> createExpression)
+        {
+            var argParam = Expression.Parameter(typeof(TArgs), "arg");
+            var logParam = Expression.Parameter(typeof(List<string>), "log");
+            var valueParam = Expression.Parameter(typeof(TResult), "result");
+            var errorParam = Expression.Parameter(typeof(Exception), "error");
+            var exParam = Expression.Parameter(typeof(Exception), "ex");
+            var addMethod = logParam.Type.GetMethod("Add", new[] { typeof(string) });
+            var logAndResultCtor = typeof(LogAndResult<TResult>).GetConstructor(new[] { typeof(List<string>), typeof(TResult), typeof(Exception) });
+
+            var getLogEntry = new Func<Expression, string, Expression>((e, s) => Expression.Block(Expression.Call(logParam, addMethod, Expression.Constant(s, typeof(string))), e));
+            var entryParam = Expression.Parameter(typeof(string));
+            var append = Expression.Lambda<Action<string>>(Expression.Call(logParam, logParam.Type.GetMethod("Add"), entryParam), entryParam);
+
+            // NB: InvalidProgramException when we expose the Lambda directly to the caller; e.g. repro on ForEach_Compile_Pattern1.
+            //     This is likely due to the Invoke(Lambda) optimization. VSadov may already have solved (cf. recursive lambdas) but
+            //     needs to be double-checked for this particular case. Simply #define REPRO to see it.
+#if REPRO
+            var body = createExpression(getLogEntry, append);
+#else
+            var appendVar = Expression.Parameter(append.Type);
+            var body = createExpression(getLogEntry, appendVar, argParam);
+#endif
+
+            var res =
+                Expression.Lambda<Func<TArgs, LogAndResult<TResult>>>(
+                    Expression.Block(
+                        new[]
+                        {
+                            logParam, valueParam, errorParam,
+#if !REPRO
+                            appendVar
+#endif
+                        },
+#if !REPRO
+                        Expression.Assign(appendVar, append),
+#endif
+                        Expression.Assign(logParam, Expression.New(typeof(List<string>))),
+                        Expression.TryCatch(
+                            Expression.Block(typeof(void),
+                                Expression.Assign(valueParam, body)
+                            ),
+                            Expression.Catch(exParam,
+                                Expression.Block(typeof(void),
+                                    Expression.Assign(errorParam, exParam)
+                                )
+                            )
+                        ),
+                        Expression.New(logAndResultCtor, logParam, valueParam, errorParam)
+                    ),
+                    argParam
                 );
 
             return res;
