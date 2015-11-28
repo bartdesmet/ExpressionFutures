2d1fb60bffd0971068fb2cf10f5afce576ed9374
Bart De Smet <bartde@microsoft.com>
2015-11-27 22:13:58 -0800

    Adding some notes.

--- CSharpExpressions/Microsoft.CSharp.Expressions/Microsoft/CSharp/Expressions/ConditionalAccessCSharpExpression.cs
+++ CSharpExpressions/Microsoft.CSharp.Expressions/Microsoft/CSharp/Expressions/ConditionalAccessCSharpExpression.cs
@@ -28,6 +28,31 @@ namespace Microsoft.CSharp.Expressions
     public partial class ConditionalAccessCSharpExpression<TExpression> : CSharpExpression
         where TExpression : Expression
     {
+        // DESIGN: This node exposes the access expression in a strongly-typed manner which can make it easier to consume,
+        //         for example in a visitor. Its use is limited to manually created conditional access expressions using
+        //         specialized factories, although we could make the general-purpose factory instantiate a generic based on
+        //         the access node type. It may have to check for valid uses anyway, so it could know what the access type
+        //         is. Note that the code emitted by the Roslyn compiler will bind to the generic factory.
+        //
+        //         However, the typing benefit of this node is rather shallow given that the access expression could be
+        //         more than just a single operation. It'd only capture the node type of the last operation. As such, it's
+        //         only useful when users construct an explicit left-associative series of conditional access expressions,
+        //         such that each access expression only involves a single operation, e.g.
+        //
+        //           ((x?.Foo)?.Bar(qux))?[baz]
+        //               ^^^^  ^^^^^^^^^  ^^^^^
+        //              Member   Call     Index
+        //
+        //         When the right-associative nature of the conditional access expression is used, access expressions can
+        //         be arbitrarily complex, so the strong typing of the access expression is shallow, e.g.
+        //
+        //           x?.Foo.Bar(qux)?[baz].Corge()
+        //             ^^^^^^^^^^^^^ ^^^^^^^^^^^^^
+        //                  Call         Call
+        //
+        //         As such, we may just want to do away with this typing. We could still keep some of the factory methods
+        //         to make it easier to construct conditional access nodes involving a single access operation.
+
         internal ConditionalAccessCSharpExpression(Expression receiver, ConditionalReceiver nonNullReceiver, TExpression whenNotNull)
         {
             Receiver = receiver;
@@ -239,6 +264,68 @@ namespace Microsoft.CSharp.Expressions
             {
                 throw Error.ConditionalReceiverTypeMismatch(receiverType, nonNullReceiverType);
             }
+
+            // TODO: More elaborate checking of `whenNotNull` would be possible, in particular to check the following:
+            //
+            //         - only supported operations can occur in the access expression
+            //         - the conditional receiver is used in an input position
+            //         - the conditional receiver is used exactly once
+            //
+            //       The only drawback is that those checks will involve a visit to the access expression, which doesn't have a
+            //       constant cost. When those nodes are deeply nested, we'd be going over the same tree many times, unless we
+            //       relax the check for the conditional receiver usage count to be at least once. This would also happen during
+            //       a rewrite of the tree by means of a visitor, which causes factory invocations.
+            //
+            //       Note that the expression compiler can still catch such violations during reduction by only reducing one
+            //       occurrence of each conditional receiver, therefore leaving any other occurrences unbound, which on its turn
+            //       causes a reduction error for the unbound conditional receiver (which is not a reducible node).
+            //
+            //       What are the implications of not performing those checks? Nothing unsafe will happen but the node type will
+            //       effectively capture a superset of the possibilities provided by the C# language for the construct. E.g.
+            //       you could construct a null-conditional call where the conditional receiver is used for a parameter that's
+            //       different from the receiving instance (or the `this` parameter for an extension method):
+            //
+            //         ConditionalAccess(bar, receiver, Call(foo, qux, { receiver }))  ==  foo.qux(bar) iff bar != null
+            //                                               ^^^         ~~~~~~~~
+            //                                            instance       argument
+            //
+            //       We could decide the cost of checking doesn't yield enough benefits given that it only applies to hand-
+            //       crafted expressions (i.e. the compiler will never emit a non-standard form). We'd still have to support
+            //       this behavior in future versions and consider it to have well-defined behavior, or document it as having
+            //       undefined behavior (with little predecents) from the get-go.
+            //
+            //       Finally, note that we could do away with this problem by modeling the nodes differently without relying on
+            //       a node for the conditional receiver. To do so, we'd have to introduce expressions for argument lists that
+            //       don't include the receiver, which duplicates a lot of existing LINQ nodes. In such a design, the receiver
+            //       would be implicit and always correspond to the "left-most source" in the access expression. It comes at
+            //       the cost of not being able to reuse existing LINQ nodes which always have a bound receiver, but we could
+            //       piggyback on the work to introduce C#-specific nodes for e.g. ParameterAssignment to create nodes for e.g.
+            //       argument lists. Old LINQ nodes could be considered applications of more primitive partial nodes:
+            //
+            //         Call(o, m, args)   ==  Access(o, Call(m, args))   ==  o.m(args)
+            //         Member(o, p)       ==  Access(o, Member(p))       ==  o.p
+            //         Index(o, i, args)  ==  Access(o, Index(i, args))  ==  o.i[args]
+            //         Invoke(f, args)    ==  Access(f, Invoke(args))    ==  o(args)
+            //
+            //       In this setting, we could still allow further composition of the access expressions by passing a list of
+            //       access operations in lieu of the second operand to `Access`, e.g.
+            //
+            //                    Access(o, { Member(p), Call(m, args2) })  ==  o.p.m(args2)
+            //
+            //       Substituting `Access` for `ConditionalAccess` would mean the following:
+            //
+            //         ConditionalAccess(o, { Member(p), Call(m, args2) })  ==  o?.p.m(args2)
+            //
+            //       Note that it makes for a much stranger tree representation (though arguably the current form is weird too)
+            //       with new partially applied `Member`, `Call`, `Index`, and `Invoke` constructs. Instead, we could nest them
+            //       as well, by only having one (inner-most, left-most) access-unbound node in the access expression:
+            //
+            //         ConditionalAccess(o, Call(Member(   p), m, args2))
+            //                                          ^^
+            //                                         hole
+            //
+            //       With this form, the check is still expensive, because we need to check for exactly one unbound form in the
+            //       source chain of the access expression.
 
             // DESIGN: We could make instances of type ConditionalAccessCSharpExpression<TExpression> and/or specialized subtypes
             //         if we decide to keep those. However, it may look strange if `whenNotNull` is not just a single access but
