b6e6fdac0f060858bbfd09176a08e90ed60b5bbb
Bart De Smet <bartde@microsoft.com>
2015-11-29 15:35:31 -0800

    Adding a graphical RoslynPad tool for quick testing.

---
+++ CSharpExpressions/RoslynPad/MainForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace RoslynPad
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnEval = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.cmbProgs = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.cmbProgs);
+            this.splitContainer1.Panel1.Controls.Add(this.btnEval);
+            this.splitContainer1.Panel1.Controls.Add(this.txtCode);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtResult);
+            this.splitContainer1.Size = new System.Drawing.Size(778, 544);
+            this.splitContainer1.SplitterDistance = 181;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btnEval
+            // 
+            this.btnEval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnEval.Location = new System.Drawing.Point(16, 132);
+            this.btnEval.Name = "btnEval";
+            this.btnEval.Size = new System.Drawing.Size(98, 35);
+            this.btnEval.TabIndex = 2;
+            this.btnEval.Text = "Evaluate";
+            this.btnEval.UseVisualStyleBackColor = true;
+            this.btnEval.Click += new System.EventHandler(this.btnEval_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCode.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCode.Location = new System.Drawing.Point(16, 32);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(750, 93);
+            this.txtCode.TabIndex = 1;
+            this.txtCode.Text = "(Expression<Func<int>>)(() => 42)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter an expression:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResult.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtResult.Location = new System.Drawing.Point(0, 0);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(778, 359);
+            this.txtResult.TabIndex = 1;
+            // 
+            // cmbProgs
+            // 
+            this.cmbProgs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbProgs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbProgs.DropDownWidth = 200;
+            this.cmbProgs.FormattingEnabled = true;
+            this.cmbProgs.Location = new System.Drawing.Point(567, 136);
+            this.cmbProgs.Name = "cmbProgs";
+            this.cmbProgs.Size = new System.Drawing.Size(199, 28);
+            this.cmbProgs.TabIndex = 3;
+            this.cmbProgs.SelectedIndexChanged += new System.EventHandler(this.cmbProgs_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.Text = "RoslynPad";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEval;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ComboBox cmbProgs;
+    }
+}
+
---
+++ CSharpExpressions/RoslynPad/MainForm.cs
@@ -0,0 +1,101 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - November 2015
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Tests.Microsoft.CodeAnalysis.CSharp;
+
+namespace RoslynPad
+{
+    public partial class MainForm : Form
+    {
+        private IDictionary<string, string> _programs = new Dictionary<string, string>
+        {
+            {
+                "Constant",
+                "(Expression<Func<int>>)(() => 42)"
+            },
+            {
+                "Block",
+                @"(Expression<Action>)(() =>
+{
+  // Add statements here
+})"
+            },
+            {
+                "Primes",
+                @"(Expression<Func<int, List<int>>>)(max =>
+{
+  var res = new List<int>();
+
+  for (var i = 2; i <= max; i++)
+  {
+    var hasDiv = false;
+
+    for (var d = 2; d <= Math.Sqrt(i); d++)
+    {
+      if (i % d == 0)
+      {
+        hasDiv = true;
+        break;
+      }
+    }
+
+    if (!hasDiv)
+    {
+      res.Add(i);
+    }
+  }
+
+  return res;
+})"
+            }
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            cmbProgs.Items.AddRange(_programs.Keys.ToArray());
+        }
+
+        private void btnEval_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtResult.ForeColor = Color.Black;
+                txtResult.Text = TestUtilities.GetDebugView(txtCode.Text);
+            }
+            catch (InvalidProgramException ex)
+            {
+                txtResult.ForeColor = Color.Red;
+                txtResult.Text = ex.Message;
+            }
+            catch (TargetInvocationException ex)
+            {
+                txtResult.ForeColor = Color.Red;
+                txtResult.Text = "LIBRARY ERROR:\r\n\r\n" + ex.InnerException.Message;
+            }
+            catch (Exception ex)
+            {
+                txtResult.ForeColor = Color.Red;
+                txtResult.Text = "COMPILER ERROR:\r\n\r\n" + ex.ToString();
+            }
+        }
+
+        private void cmbProgs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var txt = (string)cmbProgs.SelectedItem;
+            var expr = default(string);
+            if (_programs.TryGetValue(txt, out expr))
+            {
+                txtCode.Text = expr;
+            }
+        }
+    }
+}
