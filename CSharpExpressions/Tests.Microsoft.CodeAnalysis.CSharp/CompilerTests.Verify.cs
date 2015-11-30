08436d1f616991a2787332a8b2cfec0a55e9aa47
Bart De Smet <bartde@microsoft.com>
2015-11-29 20:29:47 -0800

    Verified tests for Try.

--- CSharpExpressions/Tests.Microsoft.CodeAnalysis.CSharp/CompilerTests.Verify.cs
+++ CSharpExpressions/Tests.Microsoft.CodeAnalysis.CSharp/CompilerTests.Verify.cs
@@ -249,6 +249,15 @@ namespace Tests.Microsoft.CodeAnalysis.CSharp
             // Lock
             public override void CompilerTest_2CF2_18B2() => OK();
             public override void CompilerTest_CD60_A086() => OK();
+
+            // Try
+            public override void CompilerTest_880F_A24B() => OK();
+            public override void CompilerTest_19B3_485B() => OK(); // REVIEW: Test is of type System.Exception; we'd expect a more general catch (see 8.10).
+            public override void CompilerTest_0662_485B() => OK();
+            public override void CompilerTest_F63E_8707() => OK();
+            public override void CompilerTest_02EE_D49C() => OK();
+            public override void CompilerTest_1C02_6E0D() => OK();
+            public override void CompilerTest_744C_C5E7() => OK();
         }
     }
 }
