2dc75d3a1864f951c0e2d2e7345450dc2da731c7
Bart De Smet <bartde@microsoft.com>
2015-12-15 16:16:35 -0800

    Test harness for expression optimizers.

---
+++ CSharpExpressions/Tests.Microsoft.CSharp.Expressions/OptimizerTests.Generated.cs
@@ -0,0 +1,21 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - December 2015
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+
+namespace Tests
+{
+    partial class OptimizerTests
+    {
+		[TestMethod]
+		public void Optimizer_0()
+		{
+			var expression = Expression.Block(Expression.Empty());
+			var expected = Expression.Empty();
+			AssertOptimize(expression, expected);
+		}
+
+    }
+}
---
+++ CSharpExpressions/Tests.Microsoft.CSharp.Expressions/OptimizerTests.cs
@@ -0,0 +1,32 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - December 2015
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+
+namespace Tests
+{
+    [TestClass]
+    public partial class OptimizerTests
+    {
+        [TestMethod]
+        public void Optimizer_Null()
+        {
+            AssertOptimize(null, null);
+        }
+
+        private void AssertOptimize(Expression expression, Expression expected)
+        {
+            var actual = expression.Optimize();
+
+            var actualView = actual.DebugView();
+            var expectedView = expected.DebugView();
+
+            var actualStr = actualView?.ToString();
+            var expectedStr = expectedView?.ToString();
+
+            Assert.AreEqual(expectedStr, actualStr);
+        }
+    }
+}
