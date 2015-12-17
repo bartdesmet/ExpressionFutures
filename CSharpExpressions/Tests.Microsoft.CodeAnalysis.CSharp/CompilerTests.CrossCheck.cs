7375ba473e81e67ea59c6dac4431bcecff07de15
Bart De Smet <bartde@microsoft.com>
2015-12-17 10:09:57 -0800

    Minor cosmetics.

--- CSharpExpressions/Tests.Microsoft.CodeAnalysis.CSharp/CompilerTests.CrossCheck.cs
+++ CSharpExpressions/Tests.Microsoft.CodeAnalysis.CSharp/CompilerTests.CrossCheck.cs
@@ -4,10 +4,6 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 
 namespace Tests.Microsoft.CodeAnalysis.CSharp
 {
