61b1648fa646c45654387d35e162657f8ab443b8
Bart De Smet <bartde@microsoft.com>
2020-05-05 13:47:44 -0700

    Port GetSubArray helper.

--- CSharpExpressions/Microsoft.CSharp.Expressions/System/Runtime/CompilerServices/RuntimeOpsEx.cs
+++ CSharpExpressions/Microsoft.CSharp.Expressions/System/Runtime/CompilerServices/RuntimeOpsEx.cs
@@ -58,6 +58,44 @@ namespace System.Runtime.CompilerServices
             return functionalOp(ref obj);
         }
 
+        /// <summary>
+        /// Slices the specified array using the specified range.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the array.</typeparam>
+        /// <param name="array">The array to slice.</param>
+        /// <param name="range">An object that determines the portion of <paramref name="array"/> to include in the slice.</param>
+        /// <returns>The subarray defined by <paramref name="range"/>.</returns>
+        public static T[] GetSubArray<T>(T[] array, Range range)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            (int offset, int length) = range.GetOffsetAndLength(array.Length);
+
+            if (typeof(T).IsValueType || typeof(T[]) == array.GetType())
+            {
+                // We know the type of the array to be exactly T[].
+
+                if (length == 0)
+                {
+                    return Array.Empty<T>();
+                }
+
+                var dest = new T[length];
+                Array.Copy(array, offset, dest, 0, length);
+                return dest;
+            }
+            else
+            {
+                // The array is actually a U[] where U:T.
+                T[] dest = (T[])Array.CreateInstance(array.GetType().GetElementType()!, length);
+                Array.Copy(array, offset, dest, 0, length);
+                return dest;
+            }
+        }
+
 #if ENABLE_CALLERINFO
 
         /// <summary>
