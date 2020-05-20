a519ce971c231ac3b18df7321149c20d5d4ba07e
Bart De Smet <bartde@microsoft.com>
2020-05-19 22:55:24 -0700

    Add initial set of compiler tests for tuples.

---
+++ CSharpExpressions/Tests.Microsoft.CodeAnalysis.CSharp/CompilerTests/CSharp70.Tuples.Verify.cs
@@ -0,0 +1,21 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - May 2020
+
+namespace Tests.Microsoft.CodeAnalysis.CSharp
+{
+    partial class CompilerTests_CSharp70_Tuples
+    {
+        partial class Reviewed
+        {
+            // Literal
+            public override void CompilerTest_4091_A89E() => OK();
+            public override void CompilerTest_ACFD_39F6() => OK();
+            public override void CompilerTest_694F_FB96() => OK();
+
+            // Convert
+            public override void CompilerTest_5948_DA1F() => OK(); // NB: Nullable conversion uses Convert
+            public override void CompilerTest_D7DE_3D5E() => OK();
+        }
+    }
+}
---
+++ CSharpExpressions/Tests.Microsoft.CodeAnalysis.CSharp/CompilerTests/CSharp73.TupleEquality.Verify.cs
@@ -0,0 +1,20 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - May 2020
+
+namespace Tests.Microsoft.CodeAnalysis.CSharp
+{
+    partial class CompilerTests_CSharp73_TupleEquality
+    {
+        partial class Reviewed
+        {
+            // Non-null
+            public override void CompilerTest_41C6_D6EC() => OK();
+            public override void CompilerTest_1183_EE62() => OK();
+
+            // Null
+            public override void CompilerTest_E4CE_8C7A() => OK();
+            public override void CompilerTest_0B1F_2641() => OK();
+        }
+    }
+}
