81eb96ee1188e49e8d953ad078f3e80ddd4a3d26
Bart J.F. De Smet <bartde@microsoft.com>
2021-12-07 17:33:17 -0800

    Add one first compiler test for await using.

---
+++ CSharpExpressions/Tests.Microsoft.CodeAnalysis.CSharp/CompilerTests/CSharp80.AwaitUsing.Verify.cs
@@ -0,0 +1,14 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - December 2021
+
+namespace Tests.Microsoft.CodeAnalysis.CSharp
+{
+    partial class CompilerTests_CSharp80_AwaitUsing
+    {
+        partial class Reviewed
+        {
+            public override void CompilerTest_A100_D276() => OK();
+        }
+    }
+}
