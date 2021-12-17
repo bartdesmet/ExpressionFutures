0a2b97331580906f24be98c430ee95831737d268
Bart J.F. De Smet <bartde@microsoft.com>
2021-12-16 17:52:20 -0800

    Add some tests for pattern is expressions.

---
+++ CSharpExpressions/Tests.Microsoft.CodeAnalysis.CSharp/ReducingTests/CSharp70.IsExpression.Verify.cs
@@ -0,0 +1,15 @@
+﻿// Prototyping extended expression trees for C#.
+//
+// bartde - November 2015
+
+namespace Tests.Microsoft.CodeAnalysis.CSharp
+{
+    partial class CompilerTests_CSharp70_IsExpression_Reducing
+    {
+        partial class Reviewed
+        {
+            public override void CompilerTest_9704_1B54() => OK(); // REVIEW: The top-level compiler-generated block is awkward.
+            public override void CompilerTest_BCC8_04B2() => OK();
+        }
+    }
+}
