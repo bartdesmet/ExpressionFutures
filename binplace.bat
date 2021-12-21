eeff03e6035d3bae989e1eddd6fe0cd059759c34
Bart J.F. De Smet <bartde@microsoft.com>
2021-12-20 16:28:17 -0800

    Revamp binplace.bat.

--- binplace.bat
+++ binplace.bat
@@ -13,26 +13,25 @@ copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\System.Composition.Runtime.dll" 
 copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\System.Composition.TypedParts.dll" "Binaries\"
 copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\System.Reflection.Metadata.dll" "Binaries\"
 copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\System.Runtime.CompilerServices.Unsafe.dll" "Binaries\"
-
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.dll" "Binaries\"
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.xml" "Binaries\"
-
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.Scripting.dll" "Binaries\"
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.Scripting.xml" "Binaries\"
-
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.Workspaces.dll" "Binaries\"
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.Workspaces.xml" "Binaries\"
-
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.CSharp.dll" "Binaries\"
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.CSharp.xml" "Binaries\"
-
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.CSharp.Scripting.dll" "Binaries\"
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.CSharp.Scripting.xml" "Binaries\"
-
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.CSharp.Workspaces.dll" "Binaries\"
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.CodeAnalysis.CSharp.Workspaces.xml" "Binaries\"
-
-copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.Bcl.AsyncInterfaces.dll" "Binaries\"
 copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\System.Threading.Tasks.Extensions.dll" "Binaries\"
+copy "%1\Roslyn.VisualStudio.Setup\Debug\net472\Microsoft.Bcl.AsyncInterfaces.dll" "Binaries\"
+
+copy "%1\Microsoft.CodeAnalysis\Debug\netstandard2.0\Microsoft.CodeAnalysis.dll" "Binaries\"
+copy "%1\Microsoft.CodeAnalysis\Debug\netstandard2.0\Microsoft.CodeAnalysis.xml" "Binaries\"
+
+copy "%1\Microsoft.CodeAnalysis.Scripting\Debug\netstandard2.0\Microsoft.CodeAnalysis.Scripting.dll" "Binaries\"
+copy "%1\Microsoft.CodeAnalysis.Scripting\Debug\netstandard2.0\Microsoft.CodeAnalysis.Scripting.xml" "Binaries\"
+
+copy "%1\Microsoft.CodeAnalysis.Workspaces\Debug\netstandard2.0\Microsoft.CodeAnalysis.Workspaces.dll" "Binaries\"
+copy "%1\Microsoft.CodeAnalysis.Workspaces\Debug\netstandard2.0\Microsoft.CodeAnalysis.Workspaces.xml" "Binaries\"
+
+copy "%1\Microsoft.CodeAnalysis.CSharp\Debug\netstandard2.0\Microsoft.CodeAnalysis.CSharp.dll" "Binaries\"
+copy "%1\Microsoft.CodeAnalysis.CSharp\Debug\netstandard2.0\Microsoft.CodeAnalysis.CSharp.xml" "Binaries\"
+
+copy "%1\Microsoft.CodeAnalysis.CSharp.Scripting\Debug\netstandard2.0\Microsoft.CodeAnalysis.CSharp.Scripting.dll" "Binaries\"
+copy "%1\Microsoft.CodeAnalysis.CSharp.Scripting\Debug\netstandard2.0\Microsoft.CodeAnalysis.CSharp.Scripting.xml" "Binaries\"
+
+copy "%1\Microsoft.CodeAnalysis.CSharp.Workspaces\Debug\netstandard2.0\Microsoft.CodeAnalysis.CSharp.Workspaces.dll" "Binaries\"
+copy "%1\Microsoft.CodeAnalysis.CSharp.Workspaces\Debug\netstandard2.0\Microsoft.CodeAnalysis.CSharp.Workspaces.xml" "Binaries\"
 
 :exit
