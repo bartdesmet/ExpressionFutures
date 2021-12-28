9fb4bde0e7206e0684f8c0b5b288b40e4150a096
Bart J.F. De Smet <bartde@microsoft.com>
2021-12-27 22:36:14 -0800

    Add more tests for With.

--- CSharpExpressions/Tests.Microsoft.CSharp.Expressions/WithTests.cs
+++ CSharpExpressions/Tests.Microsoft.CSharp.Expressions/WithTests.cs
@@ -6,6 +6,7 @@ using Microsoft.CSharp.Expressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -120,6 +121,196 @@ namespace Tests
             _ = CSharpExpression.With(p, members, personInits);
         }
 
+        [TestMethod]
+        public void With_Update()
+        {
+            var point1 = Expression.Parameter(typeof(Point), "p");
+
+            var x1 = CSharpExpression.MemberInitializer(typeof(Point).GetProperty(nameof(Point.X)), Expression.Constant(1));
+            var y1 = CSharpExpression.MemberInitializer(typeof(Point).GetProperty(nameof(Point.Y)), Expression.Constant(2));
+            var pointInits1 = new[] { x1, y1 };
+
+            var e1 = CSharpExpression.With(point1, clone: null, pointInits1);
+            Assert.IsNull(e1.Members);
+
+            var e2 = e1.Update(point1, new[] { x1, y1 });
+            Assert.AreSame(e1, e2);
+            Assert.IsNull(e2.Members);
+
+            var point2 = Expression.Parameter(typeof(Point), "p");
+
+            var x2 = CSharpExpression.MemberInitializer(typeof(Point).GetProperty(nameof(Point.X)), Expression.Constant(3));
+            var y2 = CSharpExpression.MemberInitializer(typeof(Point).GetProperty(nameof(Point.Y)), Expression.Constant(4));
+            var pointInits2 = new[] { x2, y2 };
+
+            var e3 = e1.Update(point2, e1.Initializers);
+            Assert.AreNotSame(e1, e3);
+            Assert.AreSame(point2, e3.Object);
+            Assert.AreSame(e1.Initializers, e3.Initializers);
+            Assert.IsNull(e3.Members);
+
+            var e4 = e1.Update(e1.Object, pointInits2);
+            Assert.AreNotSame(e1, e4);
+            Assert.AreSame(e1.Object, e4.Object);
+            Assert.IsTrue(pointInits2.SequenceEqual(e4.Initializers));
+            Assert.IsNull(e4.Members);
+        }
+
+        [TestMethod]
+        public void With_Update_AnonymousType()
+        {
+            var obj = new { Name = "Bart", Age = 21 };
+
+            var nameProperty = obj.GetType().GetProperty(nameof(obj.Name));
+            var ageProperty = obj.GetType().GetProperty(nameof(obj.Age));
+
+            var p1 = Expression.Parameter(obj.GetType(), "p");
+
+            var name1 = CSharpExpression.MemberInitializer(nameProperty, Expression.Constant("Bart"));
+            var age1 = CSharpExpression.MemberInitializer(ageProperty, Expression.Constant(21));
+            var personInits1 = new[] { name1, age1 };
+
+            var members = new MemberInfo[] { nameProperty, ageProperty };
+
+            var e1 = CSharpExpression.With(p1, members, personInits1);
+            Assert.IsTrue(members.SequenceEqual(e1.Members));
+
+            var e2 = e1.Update(p1, new[] { name1, age1 });
+            Assert.AreSame(e1, e2);
+            Assert.IsTrue(members.SequenceEqual(e2.Members));
+
+            var p2 = Expression.Parameter(obj.GetType(), "p");
+
+            var name2 = CSharpExpression.MemberInitializer(nameProperty, Expression.Constant("Homer"));
+            var age2 = CSharpExpression.MemberInitializer(ageProperty, Expression.Constant(42));
+            var personInits2 = new[] { name2, age2 };
+
+            var e3 = e1.Update(p2, e1.Initializers);
+            Assert.AreNotSame(e1, e3);
+            Assert.AreSame(p2, e3.Object);
+            Assert.AreSame(e1.Initializers, e3.Initializers);
+            Assert.IsTrue(members.SequenceEqual(e3.Members));
+
+            var e4 = e1.Update(e1.Object, personInits2);
+            Assert.AreNotSame(e1, e4);
+            Assert.AreSame(e1.Object, e4.Object);
+            Assert.IsTrue(personInits2.SequenceEqual(e4.Initializers));
+            Assert.IsTrue(members.SequenceEqual(e4.Members));
+        }
+
+        [TestMethod]
+        public void With_Visitor()
+        {
+            var x = CSharpExpression.MemberInitializer(typeof(Point).GetProperty(nameof(Point.X)), Expression.Constant(1));
+            var y = CSharpExpression.MemberInitializer(typeof(Point).GetProperty(nameof(Point.Y)), Expression.Constant(2));
+            var pointInits = new[] { x, y };
+
+            var point = Expression.Parameter(typeof(Point), "p");
+
+            var res = CSharpExpression.With(point, clone: null, pointInits);
+
+            var v = new V();
+            Assert.AreSame(res, v.Visit(res));
+            Assert.IsTrue(v.Visited);
+        }
+
+        class V : CSharpExpressionVisitor
+        {
+            public bool Visited = false;
+
+            protected internal override Expression VisitWith(WithCSharpExpression node)
+            {
+                Visited = true;
+
+                return base.VisitWith(node);
+            }
+        }
+
+        [TestMethod]
+        public void With_Compile_ClassWithClone()
+        {
+            var person = Expression.Parameter(typeof(Person), "p");
+
+            var name = CSharpExpression.MemberInitializer(typeof(Person).GetProperty(nameof(PersonNoClone.Name)), Expression.Constant("Bart"));
+            var with_name = CSharpExpression.With(person, name);
+            var f_name = Expression.Lambda<Func<Person, Person>>(with_name, person).Compile();
+            var res_name = f_name(new Person { Name = "Abraham", Age = 84 });
+            Assert.AreEqual("Bart", res_name.Name);
+            Assert.AreEqual(84, res_name.Age);
+
+            var age = CSharpExpression.MemberInitializer(typeof(Person).GetProperty(nameof(PersonNoClone.Age)), Expression.Constant(21));
+            var with_age = CSharpExpression.With(person, age);
+            var f_age = Expression.Lambda<Func<Person, Person>>(with_age, person).Compile();
+            var res_age = f_age(res_name);
+            Assert.AreEqual("Bart", res_age.Name);
+            Assert.AreEqual(21, res_age.Age);
+
+            var with_both = CSharpExpression.With(person, name, age);
+            var f_both = Expression.Lambda<Func<Person, Person>>(with_both, person).Compile();
+            var res_both = f_both(new Person { Name = "Abraham", Age = 84 });
+            Assert.AreEqual("Bart", res_both.Name);
+            Assert.AreEqual(21, res_both.Age);
+        }
+
+        [TestMethod]
+        public void With_Compile_Struct()
+        {
+            var point = Expression.Parameter(typeof(Point), "p");
+
+            var x = CSharpExpression.MemberInitializer(typeof(Point).GetProperty(nameof(Point.X)), Expression.Constant(1));
+            var with_x = CSharpExpression.With(point, x);
+            var f_x = Expression.Lambda<Func<Point, Point>>(with_x, point).Compile();
+            var res_x = f_x(new Point { X = -1, Y = -2 });
+            Assert.AreEqual(1, res_x.X);
+            Assert.AreEqual(-2, res_x.Y);
+
+            var y = CSharpExpression.MemberInitializer(typeof(Point).GetProperty(nameof(Point.Y)), Expression.Constant(2));
+            var with_y = CSharpExpression.With(point, y);
+            var f_y = Expression.Lambda<Func<Point, Point>>(with_y, point).Compile();
+            var res_y = f_y(res_x);
+            Assert.AreEqual(1, res_y.X);
+            Assert.AreEqual(2, res_y.Y);
+
+            var with_both = CSharpExpression.With(point, x, y);
+            var f_both = Expression.Lambda<Func<Point, Point>>(with_both, point).Compile();
+            var res_both = f_both(new Point { X = -1, Y = -2 });
+            Assert.AreEqual(1, res_both.X);
+            Assert.AreEqual(2, res_both.Y);
+        }
+
+        [TestMethod]
+        public void With_Compile_AnonymousType()
+        {
+            var obj = new { Name = "Abraham", Age = 84 };
+
+            var nameProperty = obj.GetType().GetProperty(nameof(obj.Name));
+            var ageProperty = obj.GetType().GetProperty(nameof(obj.Age));
+
+            var members = new MemberInfo[] { nameProperty, ageProperty };
+
+            var person = Expression.Parameter(obj.GetType(), "p");
+
+            var name = CSharpExpression.MemberInitializer(nameProperty, Expression.Constant("Bart"));
+            var with_name = CSharpExpression.With(person, members, name);
+            var f_name = Expression.Lambda(with_name, person).Compile();
+            var res_name = (dynamic)f_name.DynamicInvoke(obj);
+            Assert.AreEqual("Bart", (string)res_name.Name);
+            Assert.AreEqual(84, (int)res_name.Age);
+
+            var age = CSharpExpression.MemberInitializer(ageProperty, Expression.Constant(21));
+            var with_age = CSharpExpression.With(person, members, age);
+            var f_age = Expression.Lambda(with_age, person).Compile();
+            var res_age = (dynamic)f_age.DynamicInvoke(res_name);
+            Assert.AreEqual("Bart", (string)res_age.Name);
+            Assert.AreEqual(21, (int)res_age.Age);
+
+            var with_both = CSharpExpression.With(person, members, name, age);
+            var f_both = Expression.Lambda(with_both, person).Compile();
+            var res_both = (dynamic)f_both.DynamicInvoke(obj);
+            Assert.AreEqual("Bart", (string)res_both.Name);
+            Assert.AreEqual(21, (int)res_both.Age);
+        }
+
         class PersonNoClone
         {
             public string Name { get; set; }
@@ -144,6 +335,7 @@ namespace Tests
             public Point Clone() => this;
         }
 
+#pragma warning disable CS0649
         class Foo
         {
             public readonly int Bar;
@@ -151,5 +343,6 @@ namespace Tests
 
             public Foo Clone() => throw new Exception();
         }
+#pragma warning restore CS0649
     }
 }
