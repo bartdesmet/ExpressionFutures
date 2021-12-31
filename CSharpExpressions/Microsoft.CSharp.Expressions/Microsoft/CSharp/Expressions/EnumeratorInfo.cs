eb410d5d06c21d712d6fc4fb4c0ac4d8d2041637
Bart J.F. De Smet <bartde@microsoft.com>
2021-12-30 17:19:52 -0800

    Add initial implementation of EnumeratorInfo binding factories.

--- CSharpExpressions/Microsoft.CSharp.Expressions/Microsoft/CSharp/Expressions/EnumeratorInfo.cs
+++ CSharpExpressions/Microsoft.CSharp.Expressions/Microsoft/CSharp/Expressions/EnumeratorInfo.cs
@@ -3,8 +3,14 @@
 // bartde - December 2021
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic.Utils;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 using static System.Dynamic.Utils.ContractUtils;
 using static System.Dynamic.Utils.TypeUtils;
@@ -12,6 +18,8 @@ using static System.Linq.Expressions.ExpressionStubs;
 
 namespace Microsoft.CSharp.Expressions
 {
+    using static Helpers;
+
     /// <summary>
     /// Provides binding information for foreach operations.
     /// </summary>
@@ -258,6 +266,355 @@ namespace Microsoft.CSharp.Expressions
             CheckUsingResourceType(enumeratorType, disposeAwaitInfo, patternDispose, allowConvertToDisposable: true);
 
             return new EnumeratorInfo(isAsync, collectionType, getEnumerator, moveNext, current, currentConversion, elementType, needsDisposal, disposeAwaitInfo, patternDispose);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="EnumeratorInfo"/> object providing binding information for foreach operations.
+        /// </summary>
+        /// <param name="isAsync">A Boolean indicating whether the enumeration is asynchronous.</param>
+        /// <param name="collectionType">The type of the collection being enumerated.</param>
+        /// <returns>A <see cref="EnumeratorInfo"/> object providing binding information for foreach operations.</returns>
+        public static EnumeratorInfo EnumeratorInfo(bool isAsync, Type collectionType)
+        {
+            RequiresNotNull(collectionType, nameof(collectionType));
+            ValidateType(collectionType);
+
+            if (collectionType == typeof(string))
+            {
+                if (isAsync)
+                    throw new Exception(); // TODO - no async enumeration over string
+
+                return StringEnumeratorInfo;
+            }
+            else if (collectionType.IsArray)
+            {
+                if (isAsync)
+                    throw new Exception(); // TODO - no async enumeration over arrays
+
+                return CreateArrayEnumeratorInfo(collectionType);
+            }
+            else
+            {
+                static MethodInfo FindInstanceMethod(Type type, string name, bool allowOptionalParams)
+                {
+                    var result = default(MethodInfo);
+
+                    foreach (var m in type.GetMethods(BindingFlags.Public | BindingFlags.Instance))
+                    {
+                        if (!m.IsGenericMethod && m.Name == name)
+                        {
+                            var parameters = m.GetParametersCached();
+
+                            if (!allowOptionalParams)
+                            {
+                                if (parameters.Length == 0)
+                                {
+                                    if (result != null)
+                                        throw new Exception(); // TODO - ambiguous
+
+                                    result = m;
+                                }
+                            }
+                            else
+                            {
+                                // TODO: Implement betterness rules if there's more than one candidate?
+                                //       Add support for params.
+                                if (parameters.Length == 0 || parameters.All(p => p.HasDefaultValue))
+                                {
+                                    if (result != null)
+                                        throw new Exception(); // TODO - ambiguous
+
+                                    result = m;
+                                }
+                            }
+                        }
+                    }
+
+                    return result;
+                }
+
+                MethodInfo FindGetEnumerator(Type collectionType) => FindInstanceMethod(collectionType, isAsync ? "GetAsyncEnumerator" : "GetEnumerator", allowOptionalParams: isAsync);
+                MethodInfo FindMoveNext(Type enumeratorType) => FindInstanceMethod(enumeratorType, isAsync ? "MoveNextAsync" : "MoveNext", allowOptionalParams: isAsync);
+                MethodInfo FindDispose(Type enumeratorType) => FindInstanceMethod(enumeratorType, isAsync ? "DisposeAsync" : "Dispose", allowOptionalParams: false);
+
+                var getEnumeratorMethod = FindGetEnumerator(collectionType);
+                if (getEnumeratorMethod != null)
+                {
+                    var collectionParameter = Expression.Parameter(collectionType, "c");
+
+                    var getEnumeratorBody = CSharpExpression.Call(collectionParameter, getEnumeratorMethod, Array.Empty<ParameterAssignment>());
+                    var getEnumerator = Expression.Lambda(getEnumeratorBody, collectionParameter);
+
+                    var enumeratorType = getEnumeratorMethod.ReturnType;
+
+                    var enumeratorParameter = Expression.Parameter(enumeratorType, "e");
+
+                    var moveNextMethod = FindMoveNext(enumeratorType);
+                    if (moveNextMethod == null)
+                        throw Error.EnumeratorShouldHaveMoveNextMethod(enumeratorType);
+
+                    var moveNextReturnType = isAsync
+                        ? CSharpExpression.AwaitInfo(moveNextMethod.ReturnType).Type
+                        : moveNextMethod.ReturnType;
+
+                    if (moveNextReturnType != typeof(bool))
+                        throw new Exception(); // TODO
+
+                    var moveNextBody = CSharpExpression.Call(enumeratorParameter, moveNextMethod, Array.Empty<ParameterAssignment>());
+                    var moveNext = Expression.Lambda(moveNextBody, enumeratorParameter);
+
+                    var currentProperty = enumeratorType.GetProperty(nameof(IEnumerator.Current), BindingFlags.Public | BindingFlags.Instance);
+                    if (currentProperty == null || currentProperty.GetGetMethod(nonPublic: false) == null)
+                        throw Error.EnumeratorShouldHaveCurrentProperty(enumeratorType);
+
+                    var currentParameter = Expression.Parameter(currentProperty.PropertyType, "x");
+                    var currentConversion = Expression.Lambda(currentParameter, currentParameter);
+
+                    var elementType = currentProperty.PropertyType;
+
+                    bool needsDisposal = false;
+                    LambdaExpression patternDispose = null;
+                    AwaitInfo disposeAwaitInfo = null;
+
+                    bool CanConvertToDisposeInterface()
+                    {
+                        var disposeInterface = isAsync ? typeof(IAsyncDisposable) : typeof(IDisposable);
+                        return HasReferenceConversion(enumeratorType, disposeInterface); // REVIEW - Roslyn checks for implicit conversion
+                    }
+
+                    if ((!enumeratorType.IsSealed && !isAsync) || CanConvertToDisposeInterface())
+                    {
+                        needsDisposal = true;
+
+                        if (isAsync)
+                        {
+                            disposeAwaitInfo = AwaitInfo(typeof(ValueTask<bool>));
+                        }
+                    }
+                    else
+                    {
+                        var dispose = FindDispose(enumeratorType);
+
+                        if (dispose != null)
+                        {
+                            needsDisposal = true;
+                            patternDispose = Expression.Lambda(Expression.Call(enumeratorParameter, dispose), enumeratorParameter);
+
+                            if (isAsync)
+                            {
+                                disposeAwaitInfo = AwaitInfo(dispose.ReturnType);
+                            }
+                        }
+                    }
+
+                    return new EnumeratorInfo(
+                        isAsync,
+                        collectionType,
+                        getEnumerator,
+                        moveNext,
+                        currentProperty,
+                        currentConversion,
+                        elementType,
+                        needsDisposal,
+                        disposeAwaitInfo,
+                        patternDispose);
+                }
+                else
+                {
+                    var enumerableInterface = isAsync ? typeof(IAsyncEnumerable<>) : typeof(IEnumerable<>);
+
+                    // NB: We don't check for implicit conversions to IE<T> or IE; the caller is responsible to insert a convert if needed.
+                    //     As such, we limit the checks to checks for implemented interfaces on the collection type.
+                    var collectionInterfaceTypes = collectionType.GetInterfaces();
+
+                    var enumerableOfT = default(Type);
+                    var enumerable = default(Type);
+                    foreach (var ifType in collectionInterfaceTypes)
+                    {
+                        if (ifType.IsGenericType && ifType.GetGenericTypeDefinition() == enumerableInterface)
+                        {
+                            if (enumerableOfT != null && ifType != enumerableOfT)
+                            {
+                                throw Error.MoreThanOneIEnumerableFound(collectionType);
+                            }
+
+                            enumerableOfT = ifType;
+                        }
+                        else if (!isAsync && ifType == typeof(IEnumerable))
+                        {
+                            enumerable = ifType;
+                        }
+                    }
+
+                    var enumerableType = enumerableOfT ?? enumerable;
+
+                    if (enumerableType != null)
+                    {
+                        return CreateEnumerableEnumeratorInfo(enumerableType);
+                    }
+                }
+
+                throw Error.NoEnumerablePattern(collectionType);
+            }
+        }
+
+        private static EnumeratorInfo s_stringEnumeratorInfo;
+        private static EnumeratorInfo StringEnumeratorInfo => s_stringEnumeratorInfo ??= CreateStringEnumeratorInfo();
+
+        private static EnumeratorInfo CreateStringEnumeratorInfo()
+        {
+            var getEnumeratorMethod = typeof(string).GetMethod(nameof(string.GetEnumerator));
+
+            // NB: See remarks on array. We'll lower using a For loop but populate info nonetheless.
+
+            var collectionParameter = Expression.Parameter(typeof(string), "s");
+            var getEnumeratorBody = Expression.Call(collectionParameter, getEnumeratorMethod);
+            var getEnumerator = Expression.Lambda(getEnumeratorBody, collectionParameter);
+
+            var enumeratorType = getEnumeratorBody.Type;
+            var enumeratorVariable = Expression.Variable(enumeratorType, "e");
+
+            var moveNextMethod = enumeratorType.GetMethod(nameof(IEnumerator.MoveNext), BindingFlags.Public | BindingFlags.Instance, binder: null, Type.EmptyTypes, modifiers: null);
+            var moveNext = Expression.Lambda(Expression.Call(enumeratorVariable, moveNextMethod), enumeratorVariable);
+
+            var currentProperty = enumeratorType.GetProperty(nameof(IEnumerator.Current), BindingFlags.Public | BindingFlags.Instance);
+
+            var currentVariable = Expression.Parameter(currentProperty.PropertyType, "c");
+            var currentConversion = Expression.Lambda(CreateConvert(currentVariable, typeof(char)), currentVariable);
+
+            return new EnumeratorInfo(
+                isAsync: false,
+                collectionType: typeof(string),
+                getEnumerator,
+                moveNext,
+                currentProperty,
+                currentConversion,
+                elementType: typeof(char),
+                needsDisposal: true,
+                disposeAwaitInfo: null,
+                patternDispose: null);
+        }
+
+        private static MethodInfo s_IEnumerable_GetEnumerator;
+        private static MethodInfo IEnumerable_GetEnumerator => s_IEnumerable_GetEnumerator ??= typeof(IEnumerable).GetMethod(nameof(IEnumerable.GetEnumerator));
+
+        private static MethodInfo s_IEnumerator_MoveNext;
+        private static MethodInfo IEnumerator_MoveNext => s_IEnumerator_MoveNext ??= typeof(IEnumerator).GetMethod(nameof(IEnumerator.MoveNext));
+        
+        private static PropertyInfo s_IEnumerator_Current;
+        private static PropertyInfo IEnumerator_Current => s_IEnumerator_Current ??= typeof(IEnumerator).GetProperty(nameof(IEnumerator.Current));
+
+        private static EnumeratorInfo CreateArrayEnumeratorInfo(Type arrayType)
+        {
+            var elementType = arrayType.GetElementType();
+
+            // NB: In practice, we won't need some of these because we'll reduce ForEach over arrays to
+            //     use For loops instead. However, we'll pretend to go through IEnumerable interfaces
+            //     and be consistent with the enumerator info emitted by Roslyn.
+
+            var arrayParameter = Expression.Parameter(arrayType, "arr");
+            var getEnumerator = Expression.Lambda(Expression.Call(arrayParameter, IEnumerable_GetEnumerator), arrayParameter);
+
+            var enumeratorVariable = Expression.Parameter(typeof(IEnumerator), "e");
+            var moveNext = Expression.Lambda(Expression.Call(enumeratorVariable, IEnumerator_MoveNext), enumeratorVariable);
+
+            var currentVariable = Expression.Parameter(typeof(object), "c");
+            var currentConversion = Expression.Lambda(CreateConvert(currentVariable, elementType), currentVariable);
+
+            return new EnumeratorInfo(
+                isAsync: false,
+                collectionType: arrayType,
+                getEnumerator,
+                moveNext,
+                IEnumerator_Current,
+                currentConversion,
+                elementType,
+                needsDisposal: true,
+                disposeAwaitInfo: null,
+                patternDispose: null);
+        }
+
+        private static EnumeratorInfo s_nonGenericEnumeratorInfo;
+        private static EnumeratorInfo NonGenericEnumeratorInfo => s_nonGenericEnumeratorInfo ??= CreateNonGenericEnumeratorInfo();
+
+        private static EnumeratorInfo CreateNonGenericEnumeratorInfo()
+        {
+            var collectionParameter = Expression.Parameter(typeof(IEnumerable), "c");
+            var getEnumeratorBody = Expression.Call(collectionParameter, IEnumerable_GetEnumerator);
+            var getEnumerator = Expression.Lambda(getEnumeratorBody, collectionParameter);
+
+            var enumeratorType = getEnumeratorBody.Type;
+            var enumeratorVariable = Expression.Variable(enumeratorType, "e");
+
+            var moveNext = Expression.Lambda(Expression.Call(enumeratorVariable, IEnumerator_MoveNext), enumeratorVariable);
+
+            var currentProperty = IEnumerator_Current;
+
+            var currentVariable = Expression.Parameter(typeof(object), "o");
+            var currentConversion = Expression.Lambda(currentVariable, currentVariable);
+
+            return new EnumeratorInfo(
+                isAsync: false,
+                collectionType: typeof(IEnumerable),
+                getEnumerator,
+                moveNext,
+                currentProperty,
+                currentConversion,
+                elementType: typeof(object),
+                needsDisposal: true,
+                disposeAwaitInfo: null,
+                patternDispose: null);
+        }
+
+        private static EnumeratorInfo CreateEnumerableEnumeratorInfo(Type enumerableType)
+        {
+            if (enumerableType == typeof(IEnumerable))
+            {
+                return NonGenericEnumeratorInfo;
+            }
+
+            var genericEnumerable = enumerableType.GetGenericTypeDefinition();
+
+            var isAsync = genericEnumerable == typeof(IAsyncEnumerable<>);
+
+            var elementType = enumerableType.GetGenericArguments()[0];
+
+            var collectionParameter = Expression.Parameter(enumerableType, "c");
+
+            var getEnumeratorMethod = isAsync
+                ? enumerableType.GetNonGenericMethod(nameof(IAsyncEnumerable<object>.GetAsyncEnumerator), BindingFlags.Public | BindingFlags.Instance, new[] { typeof(CancellationToken) })
+                : enumerableType.GetNonGenericMethod(nameof(IEnumerable<object>.GetEnumerator), BindingFlags.Public | BindingFlags.Instance, Type.EmptyTypes);
+
+            var getEnumeratorBody = Expression.Call(collectionParameter, getEnumeratorMethod);
+            var getEnumerator = Expression.Lambda(getEnumeratorBody, collectionParameter);
+
+            var enumeratorType = getEnumeratorBody.Type;
+            var enumeratorVariable = Expression.Variable(enumeratorType, "e");
+
+            var moveNextMethod = isAsync
+                ? enumerableType.GetNonGenericMethod(nameof(IAsyncEnumerator<object>.MoveNextAsync), BindingFlags.Public | BindingFlags.Instance, Type.EmptyTypes)
+                : IEnumerator_MoveNext;
+
+            var moveNext = Expression.Lambda(Expression.Call(enumeratorVariable, moveNextMethod), enumeratorVariable);
+
+            var currentProperty = enumeratorType.GetProperty(nameof(IEnumerator.Current));
+
+            var currentVariable = Expression.Parameter(elementType, "o");
+            var currentConversion = Expression.Lambda(currentVariable, currentVariable);
+
+            var disposeAwaitInfo = isAsync ? AwaitInfo(typeof(ValueTask<bool>)) : null;
+
+            return new EnumeratorInfo(
+                isAsync,
+                collectionType: enumerableType,
+                getEnumerator,
+                moveNext,
+                currentProperty,
+                currentConversion,
+                elementType,
+                needsDisposal: true,
+                disposeAwaitInfo,
+                patternDispose: null);
         }
     }
 
