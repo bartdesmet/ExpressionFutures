f7d68a2b7fd17becf4cd759b810cc3025e93d02e
Bart J.F. De Smet <bartde@microsoft.com>
2022-09-05 14:22:00 -0700

    Remove some dead code.

--- CSharpExpressions/Microsoft.CSharp.Expressions/System/Linq/Expressions/Compiler/Stubs.Manual.cs
+++ CSharpExpressions/Microsoft.CSharp.Expressions/System/Linq/Expressions/Compiler/Stubs.Manual.cs
@@ -40,54 +40,23 @@ namespace System.Linq.Expressions
             }
         }
 
-#if LINQ
-        private static readonly PropertyInfo s_InvocationExpression_LambdaOperand = typeof(InvocationExpression).GetProperty("LambdaOperand", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
-
-        public static LambdaExpression LambdaOperand(this InvocationExpression expression)
-        {
-            try
-            {
-                return (LambdaExpression)s_InvocationExpression_LambdaOperand.GetValue(expression);
-            }
-            catch (TargetInvocationException ex)
-            {
-                throw ex.InnerException;
-            }
-        }
-#endif
-
         public static Expression<T> CreateExpression<T>(Expression body, string? name, bool tailCall, ReadOnlyCollection<ParameterExpression> parameters)
         {
-#if LINQ
-            return new Expression<T>(body, name, tailCall, parameters);
-#else
             return Expression.Lambda<T>(body, name, tailCall, parameters);
-#endif
         }
 
         public static IndexExpression CreateIndexExpression(Expression instance, PropertyInfo indexer, IList<Expression> arguments)
         {
-#if LINQ
-            return new IndexExpression(instance, indexer, arguments);
-#else
             return Expression.MakeIndex(instance, indexer, arguments);
-#endif
         }
 
         public static BinaryExpression CreateAssignBinaryExpression(Expression left, Expression right)
         {
-#if LINQ
-            return new AssignBinaryExpression(left, right);
-#else
             return Expression.Assign(left, right);
-#endif
         }
 
         public static NewExpression CreateNewExpression(ConstructorInfo constructor, IList<Expression> arguments, ReadOnlyCollection<MemberInfo> members)
         {
-#if LINQ
-            return new NewExpression(constructor, arguments, members);
-#else
             if (members == null)
             {
                 return Expression.New(constructor, arguments);
@@ -96,61 +65,36 @@ namespace System.Linq.Expressions
             {
                 return Expression.New(constructor, arguments, members);
             }
-#endif
         }
 
         public static UnaryExpression CreateUnaryExpression(ExpressionType nodeType, Expression expression, Type type, MethodInfo method)
         {
-#if LINQ
-            return new UnaryExpression(nodeType, expression, type, method);
-#else
             return Expression.MakeUnary(nodeType, expression, type, method);
-#endif
         }
 
         public static LoopExpression CreateLoopExpression(Expression body, LabelTarget @break, LabelTarget @continue)
         {
-#if LINQ
-            return new LoopExpression(body, @break, @continue);
-#else
             return Expression.Loop(body, @break, @continue);
-#endif
         }
 
         public static SwitchCase CreateSwitchCase(Expression body, ReadOnlyCollection<Expression> testValues)
         {
-#if LINQ
-            return new SwitchCase(body, testValues);
-#else
             return Expression.SwitchCase(body, testValues);
-#endif
         }
 
         public static SwitchExpression CreateSwitchExpression(Type type, Expression switchValue, Expression defaultBody, MethodInfo comparison, ReadOnlyCollection<SwitchCase> cases)
         {
-#if LINQ
-            return new SwitchExpression(type, switchValue, defaultBody, comparison, cases);
-#else
             return Expression.Switch(type, switchValue, defaultBody, comparison, cases);
-#endif
         }
 
         public static TryExpression CreateTryExpression(Type type, Expression body, Expression @finally, Expression fault, ReadOnlyCollection<CatchBlock> handlers)
         {
-#if LINQ
-            return new TryExpression(type, body, @finally, fault, handlers);
-#else
             return Expression.MakeTry(type, body, @finally, fault, handlers);
-#endif
         }
 
         public static BlockExpression CreateSpilledExpressionBlock(IList<Expression> expressions)
         {
-#if LINQ
-            return new SpilledExpressionBlock(expressions);
-#else
             return Expression.Block(expressions);
-#endif
         }
     }
 }
