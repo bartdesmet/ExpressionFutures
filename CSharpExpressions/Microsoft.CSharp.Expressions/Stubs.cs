325ed7be44804be282967229164e56ef8c079b82
Bart J.F. De Smet <bartde@microsoft.com>
2022-09-05 09:42:05 -0700

    Remove another unused stub.

--- CSharpExpressions/Microsoft.CSharp.Expressions/Stubs.cs
+++ CSharpExpressions/Microsoft.CSharp.Expressions/Stubs.cs
@@ -13,34 +13,6 @@ namespace System.Linq.Expressions.Compiler
 {
     [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     [System.CodeDom.Compiler.GeneratedCode("Stub generator", "1.0")]
-    static partial class DelegateHelpers
-    {
-        private static Assembly s_asm = Assembly.Load("System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-        private static Type s_typ = s_asm.GetType("System.Linq.Expressions.Compiler.DelegateHelpers");
-        private static ILookup<string, MethodInfo> s_mtds = s_typ.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance).ToLookup(m => m.Name);
-
-        private static readonly Lazy<MethodInfo> s_0 = new Lazy<MethodInfo>(() => s_mtds["MakeCallSiteDelegate"].Single(m => m.IsStatic && m.GetParameters().Select(p => p.ParameterType).SequenceEqual(new Type[] { typeof(System.Collections.ObjectModel.ReadOnlyCollection<System.Linq.Expressions.Expression>), typeof(System.Type) })));
-
-        public static System.Type MakeCallSiteDelegate(System.Collections.ObjectModel.ReadOnlyCollection<System.Linq.Expressions.Expression> types, System.Type returnType)
-        {
-            try
-            {
-                var args = new object[] { types, returnType };
-                var res = s_0.Value.Invoke(null, args);
-                return (System.Type)res;
-            }
-            catch (TargetInvocationException ex)
-            {
-                throw ex.InnerException;
-            }
-        }
-
-    }
-}
-namespace System.Linq.Expressions.Compiler
-{
-    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-    [System.CodeDom.Compiler.GeneratedCode("Stub generator", "1.0")]
     partial class StackSpillerStub
     {
         private static Assembly s_asm = Assembly.Load("System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
