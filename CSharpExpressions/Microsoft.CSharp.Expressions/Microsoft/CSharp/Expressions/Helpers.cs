c9e03adfad553361ea0b00c0634178d63718b455
Bart J.F. De Smet <bartde@microsoft.com>
2022-09-06 12:39:02 -0700

    Refactor expression factory helpers.

--- CSharpExpressions/Microsoft.CSharp.Expressions/Microsoft/CSharp/Expressions/Helpers.cs
+++ CSharpExpressions/Microsoft.CSharp.Expressions/Microsoft/CSharp/Expressions/Helpers.cs
@@ -13,6 +13,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+
 using static System.Dynamic.Utils.TypeUtils;
 
 namespace Microsoft.CSharp.Expressions
@@ -299,43 +300,6 @@ namespace Microsoft.CSharp.Expressions
             }
 
             return bindings.ToReadOnlyUnsafe();
-        }
-
-        private const int MinConstInt32 = -2;
-        private const int MaxConstInt32 = 7;
-        private static ConstantExpression[] s_constInt32;
-
-        public static ConstantExpression CreateConstantInt32(int value)
-        {
-            if (value >= MinConstInt32 && value <= MaxConstInt32)
-            {
-                var index = value - MinConstInt32;
-                var consts = s_constInt32 ??= new ConstantExpression[MaxConstInt32 - MinConstInt32 + 1];
-                return consts[index] ?? (consts[index] = Expression.Constant(value));
-            }
-
-            return Expression.Constant(value);
-        }
-
-        private static ConstantExpression s_true, s_false;
-
-        public static ConstantExpression ConstantTrue => s_true ??= Expression.Constant(true);
-        public static ConstantExpression ConstantFalse => s_false ??= Expression.Constant(false);
-
-        public static ConstantExpression CreateConstantBoolean(bool value) => value ? ConstantTrue : ConstantFalse;
-
-        private static ConstantExpression s_nullConstant;
-
-        public static ConstantExpression ConstantNull => s_nullConstant ??= Expression.Constant(null, typeof(object));
-
-        public static Expression CreateConvert(Expression expr, Type type)
-        {
-            if (AreEquivalent(expr.Type, type))
-            {
-                return expr;
-            }
-
-            return Expression.Convert(expr, type);
         }
 
         public static Expression InvokeLambdaWithSingleParameter(LambdaExpression lambda, Expression argument)
